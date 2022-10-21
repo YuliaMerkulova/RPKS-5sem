f0dee4e8e67d49c9a703625481719c5d13685435
YuliaMerkulova <31349056+YuliaMerkulova@users.noreply.github.com>
2022-10-21 21:03:26 +0300

    rty read

--- Lab2/src/MainClass.java
+++ Lab2/src/MainClass.java
@@ -1,2 +1,19 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class MainClass {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        try {
+            System.out.println("Input sum of money");
+            Integer sum = Integer.valueOf(in.next());
+            System.out.println("Input values in format: num num num");
+            String values = in.nextLine();
+            HashSet<Integer> valuesSet = new HashSet<Integer>();
+        }catch (NumberFormatException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
